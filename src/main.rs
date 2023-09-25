8534e3c01b15c077edd47e3cacaf8604422be2c7
Brandon Pliska <brandon.pliska@gmail.com>
2023-09-25 14:00:51 -0700

    enhance

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-use std::convert::Infallible;
 use tokio::fs::File;
-use warp::http::Response;
-use warp::hyper::Body;
 use warp::Filter;
+use warp::Rejection;
+use warp::Reply;
+
+// These are the corrected hyper imports.
+use hyper::Body;
+use hyper::Response;
 
 #[tokio::main]
 async fn main() {
-    let mp4 = warp::path("video").and(warp::path("meow-meow-meow.mp4")).and_then(video_handler);
+    let mp4 = warp::path!("video" / String)
+        .and_then(video_handler); // Using and_then to directly handle the async function without further mapping.
 
     warp::serve(mp4).run(([127, 0, 0, 1], 3030)).await;
 }
 
-async fn video_handler() -> Result<impl warp::Reply, Infallible> {
-    let file = File::open("./video/meow-meow-meow.mp4").await.unwrap();
-    let stream = tokio_util::io::ReaderStream::new(file);
-    let body = Body::wrap_stream(stream);
+async fn video_handler(param: String) -> Result<impl Reply, Rejection> {
+    let file_path = format!("./video/{}", param);
+    match File::open(&file_path).await {
+        Ok(file) => {
+            let stream = tokio_util::io::ReaderStream::new(file); // Ensure tokio_util is in your Cargo.toml dependencies.
+            let body = Body::wrap_stream(stream);
 
-    Ok(Response::builder().header("Content-Type", "video/mp4").body(body).unwrap())
+            let response = Response::builder()
+                .header("Content-Type", "video/mp4")
+                .body(body)
+                .unwrap();
+
+            Ok(response)
+        }
+        Err(_) => {
+            // Handle the error gracefully, e.g., by returning a 404 response.
+            let response = Response::builder()
+                .status(404)
+                .body(Body::from("File not found")) // Body::from(...) is used to convert &str to Body.
+                .unwrap();
+
+            Ok(response)
+        }
+    }
 }
